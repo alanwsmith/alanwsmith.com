de1a5add1f88d5f7096b2bb35bce6cabd4b9d09f
Alan W. Smith <alan@alanwsmith.com>
2021-10-24 21:49:08 -0400

    Add OG image basic

--- site/pages/posts/[slug].js
+++ site/pages/posts/[slug].js
@@ -1,17 +1,30 @@
 import { getAllPosts, getPostBySlug } from '../../lib/api'
+import Head from 'next/head'
 
 export default function Post({ post }) {
   return (
-    <div className="post">
-      <div dangerouslySetInnerHTML={{ __html: post.body }} />
-    </div>
+    <>
+      <Head>
+        <meta
+          name="og:image"
+          content={`https://res.cloudinary.com/awsimages/image/upload/c_fit,l_text:Arial_68_bold:${encodeURIComponent(
+            post.title
+          )},w_720/fl_layer_apply,g_north_west,x_480,y_68/og-image-v3_eka6dz.png`}
+        />
+      </Head>
+      <div className="post">
+        <div dangerouslySetInnerHTML={{ __html: post.body }} />
+      </div>
+    </>
   )
 }
 
 export function getStaticProps({ params }) {
+  const post = getPostBySlug(params.slug)
+
   return {
     props: {
-      post: getPostBySlug(params.slug),
+      post,
     },
   }
 }
