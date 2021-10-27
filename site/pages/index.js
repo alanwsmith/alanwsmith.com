485c92ea2f07071f457137eb3ebfe421f75ac881
Alan W. Smith <alan@alanwsmith.com>
2021-10-26 22:07:31 -0400

    Put posts on home page

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,7 +1,35 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-export default function Home() {
-  return <div>this is a test</div>
+// export default function Home() {
+//   return <div>this is a test</div>
+// }
+
+import { getAllPosts } from '../lib/api'
+
+export default function Posts({ posts }) {
+  return (
+    <div>
+      <h1>Posts</h1>This is it{' '}
+      {posts.map((post) => {
+        return (
+          <div key={post.permalink}>
+            <Link href={post.permalink}>
+              <a>{post.title}</a>
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      posts: getAllPosts(),
+    },
+  }
 }
