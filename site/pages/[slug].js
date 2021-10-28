98914afefa2ef5467f2673995552daa5bcb4cf0a
Alan W. Smith <alan@alanwsmith.com>
2021-10-27 20:43:06 -0400

    Setup to get mdx working, but it doen't execute jsx yet

--- site/pages/[slug].js
+++ site/pages/[slug].js
@@ -1,24 +1,31 @@
 import { getAllPosts, getPostBySlug } from '../lib/api'
 import Head from 'next/head'
 import LayoutMain from '../components/LayoutMain.js'
-import { serialize } from 'next-mdx-remote/serialize'
+// import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote } from 'next-mdx-remote'
 
-export default function Post({ post }) {
+/*
+            <div
+              className="text-blue-200"
+              dangerouslySetInnerHTML={{ __html: post }}
+            />
+          */
+
+export default function Post({ frontMatter, mdxSource }) {
   return (
     <>
       <Head>
         <meta
           name="og:image"
           content={`https://res.cloudinary.com/awsimages/image/upload/c_fit,l_text:Arial_68_bold:${encodeURIComponent(
-            post.title
+            frontMatter.title
           )},w_720/fl_layer_apply,g_north_west,x_480,y_68/og-image-v3_eka6dz.png`}
         />
       </Head>
       <LayoutMain
         content={
           <div className="max-w-prose">
-            <h3 className="text-blue-300 text-3xl ">{post.title}</h3>
+            <h3 className="text-blue-300 text-3xl ">{frontMatter.title}</h3>
             <div
               className="
               border-b 
@@ -28,12 +35,11 @@ export default function Post({ post }) {
               pb-4
               mb-4"
             >
-              {post.date}
+              {frontMatter.date}
             </div>
-            <div
-              className="text-blue-200"
-              dangerouslySetInnerHTML={{ __html: post.body }}
-            />
+            <div className="text-blue-200">
+              <MDXRemote {...mdxSource} />
+            </div>
           </div>
         }
       />
@@ -41,12 +47,20 @@ export default function Post({ post }) {
   )
 }
 
-export function getStaticProps({ params }) {
-  const post = getPostBySlug(params.slug)
+export async function getStaticProps({ params }) {
+  const source = await getPostBySlug(params.slug)
+  const mdxSource = source.mdxSource
+  const frontMatter = source.data
+  console.log(source)
   return {
     props: {
-      post,
+      mdxSource,
+      frontMatter,
     },
+
+    // props: {
+    //   post,
+    // },
   }
 }
 
