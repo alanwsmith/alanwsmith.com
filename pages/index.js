688f7fd17d9334cae3941eb2b5075971929c644b
Alan W. Smith <alan@alanwsmith.com>
2021-10-29 18:42:54 -0400

    Reverted back to started from cassidoo

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+// import Header from '@components/Header'
+// import Footer from '@components/Footer'
+
+export default function Home() {
+  return <div className="container">the home page of alan w. smith</div>
+}
