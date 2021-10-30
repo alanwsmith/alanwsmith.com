34551055f84790a17389bdf478ade2c8a82548e8
Alan W. Smith <alan@alanwsmith.com>
2021-10-29 23:39:28 -0400

    Corrected link to post pages

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function BlogList({ posts }) {
               <ul>
                 {posts.map((post, index) => (
                   <li key={index}>
-                    <Link href={`${post.slug}`}>{post.title}</Link>
+                    <Link href={`/${post.slug}`}>{post.title}</Link>
                   </li>
                 ))}
               </ul>
