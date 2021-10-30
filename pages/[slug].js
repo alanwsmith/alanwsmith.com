015eb2bb3c31fb598179e7a591ce7dda9c21ab79
Alan W. Smith <alan@alanwsmith.com>
2021-10-30 14:37:04 -0400

    Progress on getting the responsive vimeo video to work

--- pages/[slug].js
+++ pages/[slug].js
@@ -7,10 +7,45 @@ import LayoutMain from '../components/LayoutMain.js'
 // <div style="padding:75.4% 0 0 0;position:relative;">
 //
 // style="position:absolute;top:0;left:0;width:100%;height:100%;"
+//
+// <style>.embed-container
+// { position: relative; padding-bottom: 56.25%;
+// height: 0; overflow: hidden; max-width: 100%; }
+// .embed-container iframe, .embed-container object,
+// .embed-container embed {
+// position: absolute; top: 0; left: 0;
+// width: 100%; height: 100%; }
+// </style>
+// <div class='embed-container'>
+// <iframe src='https://player.vimeo.com/video/66140585' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
 
 const VimeoVideo = ({ id }) => {
   return (
     <>
+      <div
+        style={{
+          position: 'relative',
+          paddingBottom: '56.25%',
+          height: '0',
+          overflow: 'hidden',
+          maxWidth: '100%',
+        }}
+      >
+        <iframe
+          style={{
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+          }}
+          src="https://player.vimeo.com/video/66140585"
+          frameborder="0"
+          webkitAllowFullScreen
+          mozAllowFullscreen
+          allowFullScreen
+        />
+      </div>
       <div
         style={{
           position: 'relative',
