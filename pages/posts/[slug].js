4fc52487fc2cc7eff14ecccbfa7cbe2918166030
Alan W. Smith <alan@alanwsmith.com>
2021-11-02 22:00:31 -0400

    Starting to build process that allows for KSUIDs

---
+++ pages/posts/[slug].js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { getMDXComponent } from 'mdx-bundler/client'
+import { getAllPosts, getSinglePost } from '../../utils/mdx_v2'
+
+const Post = ({ code, frontmatter }) => {
+  return <div>asdf</div>
+
+  /*
+  const Component = React.useMemo(() => getMDXComponent(code), [code])
+  return (
+    <>
+      <h1>{frontmatter.title}</h1>
+      <div>{frontmatter.date}</div>
+      <Component />
+    </>
+  )
+  */
+}
+
+export const getStaticProps = async ({ params }) => {
+  console.log('-- This is getStaticProps --')
+  const post = await getSinglePost(params.slug)
+  return {
+    // props: { ...post },
+    props: {},
+  }
+}
+
+export const getStaticPaths = async () => {
+  console.log('-- This is getStaticPaths --')
+  const paths = getAllPosts().map(({ slug }) => ({ params: { slug } }))
+  console.log(`-- Lenght of paths: ${paths.length}`)
+  console.log(`-- Paths: ${Object.keys(paths[0])}`)
+  console.log(`-- Slug: ${paths[0].params.slug}`)
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default Post
---
+++ utils/mdx_v2.js
@@ -0,0 +1,69 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { bundleMDX } from 'mdx-bundler'
+
+export const POSTS_PATH = path.join(process.cwd(), '_posts')
+
+export const getSourceOfFile = (fileName) => {
+  return fs.readFileSync(path.join(POSTS_PATH, fileName))
+}
+
+export const getAllPosts = () => {
+  return fs
+    .readdirSync(POSTS_PATH)
+    .filter((path) => /\.mdx?$/.test(path))
+    .map((fileName) => {
+      const source = getSourceOfFile(fileName)
+      const fileId = fileName.replace(/\.mdx?$/, '')
+      //
+      const { data } = matter(source)
+
+      // Still need this since it's passed back.
+      // Check to see if that still needs to happen.
+      // Probalby does.
+      const title = data.title
+
+      // TODO: make a scrubbed version of the title
+      // hard coding for now
+      const scrubbedTitle = 'some-new-file-title'
+
+      // TODO: Pull the actual frontmatter title
+      // after scrubbing it
+      const slug = `${fileId}-${scrubbedTitle}`
+      // const slug = fileId
+
+      // TODO: Figure out what to do if title is undefined
+      // if (title === undefined) {
+      //   console.log(fileName)
+      //   console.log(title)
+      // }
+
+      console.log(`Returning: ${slug}`)
+      console.log(`  with title: ${title}`)
+
+      return {
+        title: title,
+        slug: slug,
+      }
+    })
+}
+
+export const getSinglePost = async (slug) => {
+  console.log(`getSinglePost - ${slug}`)
+  return {}
+
+  /*
+  const slug_parts = slug.split('/')
+  console.log(`Looking for: ${slug_parts[0]}`)
+  const source = getSourceOfFile(slug_parts[0] + '.mdx')
+
+  const { code, frontmatter } = await bundleMDX(source, {
+    cwd: POSTS_PATH,
+  })
+  return {
+    frontmatter,
+    code,
+  }
+  */
+}
