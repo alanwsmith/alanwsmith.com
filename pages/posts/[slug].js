67de1ffad7e75f392f213187646be470cac81d33
Alan W. Smith <alan@alanwsmith.com>
2021-11-14 20:26:04 -0500

    Moved YouTubeVideo to its own component

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import LayoutMain from '../../components/LayoutMain.js'
 import HeaderDate from '../../components/HeaderDate.js'
 import VimeoVideo from '../../components/VimeoVideo.js'
+import YouTubeVideo from '../../components/YouTubeVideo.js'
 
 // const VimeoVideo = ({ id }) => {
 //   return (
@@ -35,38 +36,38 @@ import VimeoVideo from '../../components/VimeoVideo.js'
 //   )
 // }
 
-const YouTubeVideo = ({ id }) => {
-  return (
-    <>
-      <div
-        style={{
-          position: 'relative',
-          paddingBottom: '56.25%',
-          height: '0',
-          overflow: 'hidden',
-          maxWidth: '100%',
-        }}
-      >
-        <iframe
-          style={{
-            position: 'absolute',
-            top: '0',
-            left: '0',
-            width: '100%',
-            height: '100%',
-          }}
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${id}?controls=0`}
-          title="Video"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-    </>
-  )
-}
+// const YouTubeVideo = ({ id }) => {
+//   return (
+//     <>
+//       <div
+//         style={{
+//           position: 'relative',
+//           paddingBottom: '56.25%',
+//           height: '0',
+//           overflow: 'hidden',
+//           maxWidth: '100%',
+//         }}
+//       >
+//         <iframe
+//           style={{
+//             position: 'absolute',
+//             top: '0',
+//             left: '0',
+//             width: '100%',
+//             height: '100%',
+//           }}
+//           width="560"
+//           height="315"
+//           src={`https://www.youtube.com/embed/${id}?controls=0`}
+//           title="Video"
+//           frameBorder="0"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//           allowFullScreen
+//         ></iframe>
+//       </div>
+//     </>
+//   )
+// }
 
 const Post = ({ code, frontmatter }) => {
   const Component = React.useMemo(() => getMDXComponent(code), [code])
