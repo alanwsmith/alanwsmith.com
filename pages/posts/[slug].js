3f2072df20827f357af8b415062a726e0597919f
Alan W. Smith <alan@alanwsmith.com>
2021-11-14 20:23:49 -0500

    Moved VimeoVideo to it's own component

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,37 +4,36 @@ import { getAllPosts, getSinglePost } from '../../utils/mdx'
 import Head from 'next/head'
 import LayoutMain from '../../components/LayoutMain.js'
 import HeaderDate from '../../components/HeaderDate.js'
+import VimeoVideo from '../../components/VimeoVideo.js'
 
-const VimeoVideo = ({ id }) => {
-  return (
-    <>
-      <div
-        style={{
-          position: 'relative',
-          paddingBottom: '56.25%',
-          height: '0',
-          overflow: 'hidden',
-          maxWidth: '100%',
-        }}
-      >
-        <iframe
-          style={{
-            position: 'absolute',
-            top: '0',
-            left: '0',
-            width: '100%',
-            height: '100%',
-          }}
-          src={`https://player.vimeo.com/video/${id}`}
-          frameBorder="0"
-          webkitAllowFullScreen
-          mozAllowFullscreen
-          allowFullScreen
-        />
-      </div>
-    </>
-  )
-}
+// const VimeoVideo = ({ id }) => {
+//   return (
+//     <>
+//       <div
+//         style={{
+//           position: 'relative',
+//           paddingBottom: '56.25%',
+//           height: '0',
+//           overflow: 'hidden',
+//           maxWidth: '100%',
+//         }}
+//       >
+//         <iframe
+//           style={{
+//             position: 'absolute',
+//             top: '0',
+//             left: '0',
+//             width: '100%',
+//             height: '100%',
+//           }}
+//           src={`https://player.vimeo.com/video/${id}`}
+//           frameBorder="0"
+//           allowFullScreen
+//         />
+//       </div>
+//     </>
+//   )
+// }
 
 const YouTubeVideo = ({ id }) => {
   return (
