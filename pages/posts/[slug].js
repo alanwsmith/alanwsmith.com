ccf220875f4c50473beb9497fc2842715d932ce1
Alan W. Smith <alan@alanwsmith.com>
2021-11-14 20:29:05 -0500

    Removed comments from video components

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -7,68 +7,6 @@ import HeaderDate from '../../components/HeaderDate.js'
 import VimeoVideo from '../../components/VimeoVideo.js'
 import YouTubeVideo from '../../components/YouTubeVideo.js'
 
-// const VimeoVideo = ({ id }) => {
-//   return (
-//     <>
-//       <div
-//         style={{
-//           position: 'relative',
-//           paddingBottom: '56.25%',
-//           height: '0',
-//           overflow: 'hidden',
-//           maxWidth: '100%',
-//         }}
-//       >
-//         <iframe
-//           style={{
-//             position: 'absolute',
-//             top: '0',
-//             left: '0',
-//             width: '100%',
-//             height: '100%',
-//           }}
-//           src={`https://player.vimeo.com/video/${id}`}
-//           frameBorder="0"
-//           allowFullScreen
-//         />
-//       </div>
-//     </>
-//   )
-// }
-
-// const YouTubeVideo = ({ id }) => {
-//   return (
-//     <>
-//       <div
-//         style={{
-//           position: 'relative',
-//           paddingBottom: '56.25%',
-//           height: '0',
-//           overflow: 'hidden',
-//           maxWidth: '100%',
-//         }}
-//       >
-//         <iframe
-//           style={{
-//             position: 'absolute',
-//             top: '0',
-//             left: '0',
-//             width: '100%',
-//             height: '100%',
-//           }}
-//           width="560"
-//           height="315"
-//           src={`https://www.youtube.com/embed/${id}?controls=0`}
-//           title="Video"
-//           frameBorder="0"
-//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-//           allowFullScreen
-//         ></iframe>
-//       </div>
-//     </>
-//   )
-// }
-
 const Post = ({ code, frontmatter }) => {
   const Component = React.useMemo(() => getMDXComponent(code), [code])
   return (
