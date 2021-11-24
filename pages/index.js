207d5926e888eab9d89fcc3c3f821cbc4d9dddad
Alan W. Smith <alan@alanwsmith.com>
2021-11-23 19:15:18 -0500

    Added styles back in

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,46 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import Link from 'next/link'
+import Head from 'next/head'
 import LayoutMain from '../components/LayoutMain'
 
-export default function HomePage({ posts }) {
+export default function BlogList({ posts }) {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta property="og:title" content="alanwsmith.com" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:creator" content="@theidofalan" />
+        <meta
+          property="og:description"
+          content="The personal website of Alan W. Smith"
+        />
+        <meta property="og:url" content={`https://www.alanwsmith.com`} />
+        <meta
+          property="og:image"
+          content={`https://res.cloudinary.com/awsimages/image/upload/c_fit,l_text:Arial_68_bold:TheSiteOfAlan,w_720/fl_layer_apply,g_north_west,x_480,y_68/og-image-v3_eka6dz.png`}
+        />
+      </Head>
       <LayoutMain
         content={
           <>
-            <h1>This is the home page</h1>
-            <p>Stuff broke... I fix</p>
-            <ul>
-              {posts.map((post) => (
-                <li key={post.slug}>
-                  <Link href={`/posts/${post.slug}`}>
-                    <a>{post.title}</a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <p className="pb-4 text-blue-200 max-w-prose">
+              Hey, folks. I&apos;m Alan. Welcome to my site. I&apos;m in the
+              process of moving it. So, a bunch of stuff is broken. That&apos;s
+              all part of the idea of building in public. I&apos;ll make
+              progress over time. For now, here&apos; an alphabetical list of
+              posts on the site.
+            </p>
+            <div className="max-w-prose pt-4 border-t border-gray-400 text-blue-200">
+              <ul>
+                {posts.map((post, index) => (
+                  <li key={index}>
+                    <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </>
         }
       />
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -3,6 +3,7 @@ import path from 'path'
 import matter from 'gray-matter'
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote } from 'next-mdx-remote'
+import Head from 'next/head'
 import Link from 'next/link'
 
 const CONTENT_DIR = path.join(process.cwd(), '_posts')
@@ -16,25 +17,49 @@ import VimeoVideo from '../../components/VimeoVideo'
 export default function Post({ source, frontmatter }) {
   return (
     <>
-      <div>
-        <Link href="/">
-          <a>Home Page</a>
-        </Link>
-      </div>
+      <Head>
+        <meta property="og:title" content={frontmatter.title} />
+        <meta property="og:type" content="article" />
+        <meta name="twitter:creator" content="@theidofalan" />
+        <meta property="og:description" content="A post from Alan W. Smith" />
+        <meta
+          property="og:url"
+          content={`https://www.alanwsmith.com${frontmatter.slug}`}
+        />
+        <meta
+          property="og:image"
+          content={`https://res.cloudinary.com/awsimages/image/upload/c_fit,l_text:Arial_68_bold:${encodeURIComponent(
+            frontmatter.title
+          )},w_720/fl_layer_apply,g_north_west,x_480,y_68/og-image-v3_eka6dz.png`}
+        />
+      </Head>
       <LayoutMain
         content={
-          <>
-            <h1>{frontmatter.title}</h1>
-            <MDXRemote
-              {...source}
-              components={{
-                Checklist,
-                ReadOnlyChecklist,
-                YouTubeVideo,
-                VimeoVideo,
-              }}
-            />
-          </>
+          <div className="max-w-prose">
+            <h1 className="text-blue-300 text-3xl ">{frontmatter.title}</h1>
+            <div
+              className="
+              border-b 
+              border-gray-400 
+              text-gray-600 
+              text-xs 
+              pb-4
+              mb-4"
+            >
+              <div className="pt-1">todo: put date here</div>
+            </div>
+            <div className="text-blue-200">
+              <MDXRemote
+                {...source}
+                components={{
+                  Checklist,
+                  ReadOnlyChecklist,
+                  YouTubeVideo,
+                  VimeoVideo,
+                }}
+              />
+            </div>
+          </div>
         }
       />
     </>
