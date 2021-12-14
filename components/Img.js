3ae1fcb9aa25a219a342cc820ef8245c9db995c2
Alan W. Smith <alan@alanwsmith.com>
2021-12-13 20:33:44 -0500

    Add podcast feed

--- components/Img.js
+++ components/Img.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import acme_bag_2008_05 from '../_images/acme_bag_2008_05.jpg'
+import aws_20080426_glamor_camp_0073 from '../_images/aws_20080426_glamor_camp_0073.jpg'
 import bama_schedule_sp_20090821_misc_112136 from '../_images/bama_schedule_sp_20090821_misc_112136.png'
 import black_widow_20051026_230734a1 from '../_images/black_widow_20051026_230734a1.jpg'
 import bottle_of_change_20060313_150636a from '../_images/bottle_of_change_20060313_150636a.jpg'
@@ -14,12 +15,16 @@ import fire_eyes_1 from '../_images/fire_eyes_1.jpg'
 import glock_20060923_114208_01 from '../_images/glock_20060923_114208_01.jpg'
 import home_and_garden_skull_2009_03 from '../_images/home_and_garden_skull_2009_03.png'
 import iphone_3gs_test_photo_2009_06 from '../_images/iphone_3gs_test_photo_2009_06.jpg'
+import ir_band_photo_20060805_011554_01 from '../_images/ir_band_photo_20060805_011554_01.jpg'
+import ir_band_photo_20060805_011816_01 from '../_images/ir_band_photo_20060805_011816_01.jpg'
 import kexp_song_of_the_day_2009_06 from '../_images/kexp_song_of_the_day_2009_06.jpg'
 import last_sunday_at_reimers_img_0289 from '../_images/last_sunday_at_reimers_img_0289.jpg'
 import last_sunday_at_reimers_img_0293 from '../_images/last_sunday_at_reimers_img_0293.jpg'
 import mile_200000_20080402_snaps_0039 from '../_images/mile_200000_20080402_snaps_0039.jpg'
 import movies_cropped_2006_02 from '../_images/movies_cropped_2006_02.jpg'
 import play_button_2009_01 from '../_images/play_button_2009_01.gif'
+import plr_sample_finish from '../_images/plr_sample_finish.jpg'
+import plr_sample_start from '../_images/plr_sample_start.jpg'
 import shuttle_patch_150x166 from '../_images/shuttle_patch_150x166.png'
 import sunset_in_key_weat_20060211_191750a from '../_images/sunset_in_key_weat_20060211_191750a.jpg'
 import superfad_reel_grab_2009_01 from '../_images/superfad_reel_grab_2009_01.png'
@@ -34,6 +39,7 @@ import youtube_logo_2009_01 from '../_images/youtube_logo_2009_01.gif'
 
 const imgMap = {
   acme_bag_2008_05: acme_bag_2008_05,
+  aws_20080426_glamor_camp_0073: aws_20080426_glamor_camp_0073,
   bama_schedule_sp_20090821_misc_112136: bama_schedule_sp_20090821_misc_112136,
   black_widow_20051026_230734a1: black_widow_20051026_230734a1,
   bottle_of_change_20060313_150636a: bottle_of_change_20060313_150636a,
@@ -47,12 +53,16 @@ const imgMap = {
   glock_20060923_114208_01: glock_20060923_114208_01,
   home_and_garden_skull_2009_03: home_and_garden_skull_2009_03,
   iphone_3gs_test_photo_2009_06: iphone_3gs_test_photo_2009_06,
+  ir_band_photo_20060805_011554_01: ir_band_photo_20060805_011554_01,
+  ir_band_photo_20060805_011816_01: ir_band_photo_20060805_011816_01,
   kexp_song_of_the_day_2009_06: kexp_song_of_the_day_2009_06,
   last_sunday_at_reimers_img_0289: last_sunday_at_reimers_img_0289,
   last_sunday_at_reimers_img_0293: last_sunday_at_reimers_img_0293,
   mile_200000_20080402_snaps_0039: mile_200000_20080402_snaps_0039,
   movies_cropped_2006_02: movies_cropped_2006_02,
   play_button_2009_01: play_button_2009_01,
+  plr_sample_finish: plr_sample_finish,
+  plr_sample_start: plr_sample_start,
   shuttle_patch_150x166: shuttle_patch_150x166,
   sunset_in_key_weat_20060211_191750a: sunset_in_key_weat_20060211_191750a,
   superfad_reel_grab_2009_01: superfad_reel_grab_2009_01,
