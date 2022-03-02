69b344b22efc3a242e2512a26e88bea6c6451078
Alan Smith <alan@alanwsmith.com>
2022-03-02 17:17:36 -0500

    Added notes

--- pages/code-snippet-design-mock-ups/index.js
+++ pages/code-snippet-design-mock-ups/index.js
@@ -1,20 +1,54 @@
 import HeaderMain from '../../components/HeaderMain'
 import ContentWrapper from '../../components/ContentWrapper'
 import Sample1 from './samples/sample-1'
+import Sample2 from './samples/sample-2'
 
 export default function Page() {
   return (
     <>
+      <style jsx>
+        {`
+          h3 {
+            margin-top: 20px;
+          }
+        `}
+      </style>
+
       <HeaderMain />
       <ContentWrapper>
         <h1>Code Snippet Design Tests</h1>
         <p>
           I&apos;ve been playing around with the design of code snippets for my
-          site. These are the samples I&apos;m working with.
+          site. These are the samples I&apos;m working with. (These are not
+          currently responsive. I&apos;ll be working on that. Also, the Copy
+          buttons don&apos;t do anything since these are just mock-ups.)
         </p>
 
+        <ul>
+          <li>These mock-ups are focused on the layout</li>
+          <li>
+            The colors are a baseline. They&apos;ll be determined by the overall
+            site theme in the end
+          </li>
+        </ul>
+
         <h3>Sample 1</h3>
+        <ul>
+          <li>Pretty much a baseline sample</li>
+          <li>Only showing ling numbers by the code.</li>
+          <li>
+            Using a pointer for the output line, but it&apos;s still in the main
+            content box
+          </li>
+        </ul>
         <Sample1 />
+
+        <h3>Sample 2</h3>
+        <ul>
+          <li>Basic idea here is to split the code from the output</li>
+        </ul>
+
+        <Sample2 />
       </ContentWrapper>
     </>
   )
