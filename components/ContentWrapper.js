868c83474850a3f188f6293d354e627acdc2c59e
Alan Smith <alan@alanwsmith.com>
2022-03-02 17:17:22 -0500

    Set margin at the bottom

--- components/ContentWrapper.js
+++ components/ContentWrapper.js
@@ -1,6 +1,6 @@
 export default function ContentWrapper({ children }) {
   return (
-    <div className="max-w-screen-md mx-auto mt-4">
+    <div className="max-w-screen-md mx-auto mt-4 mb-20">
       <div>{children}</div>
     </div>
   )
