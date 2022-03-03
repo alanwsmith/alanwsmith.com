a629eed26b7e4bad4726c3ae17e3e1bf3f269fe7
Alan Smith <alan@alanwsmith.com>
2022-03-02 20:24:09 -0500

    Moved early samples later and setup baseline first sample

--- pages/code-snippet-design-mock-ups/index.js
+++ pages/code-snippet-design-mock-ups/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import HeaderMain from '../../components/HeaderMain'
 import ContentWrapper from '../../components/ContentWrapper'
 import Sample1 from './samples/sample-1'
-import Sample2 from './samples/sample-2'
+// import Sample2 from './samples/sample-2'
 
 export default function Page() {
   const [activeSample, setActiveSample] = useState(1)
@@ -10,7 +10,7 @@ export default function Page() {
   const samples = [
     <div key="s0">placeholder</div>,
     <Sample1 key="s1" />,
-    <Sample2 key="s2" />,
+    //  <Sample2 key="s2" />,
   ]
 
   const switchToSample = (sampleIndex) => {
@@ -33,22 +33,16 @@ export default function Page() {
         <h1>Code Snippet Design Tests</h1>
         <p>
           I&apos;ve been playing around with the design of code snippets for my
-          site. These are the samples I&apos;m working with. (These are not
-          currently responsive. I&apos;ll be working on that. Also, the Copy
-          buttons don&apos;t do anything since these are just mock-ups.)
+          site. These are the samples I&apos;m of what I&apos;m working on.
+          (They&apos;re not currently responsive. I&apos;ll be working on that.
+          Also, the Copy buttons don&apos;t do anything since these are just
+          mock-ups.)
         </p>
-
-        <ul>
-          <li>These mock-ups are focused on the layout</li>
-          <li>
-            The colors are a baseline. They&apos;ll be determined by the overall
-            site theme in the end
-          </li>
-        </ul>
 
         <button onClick={() => switchToSample(1)}>1</button>
         <button onClick={() => switchToSample(2)}>2</button>
 
+        <h3>Design {activeSample}</h3>
         {samples[activeSample]}
       </ContentWrapper>
     </>
--- pages/code-snippet-design-mock-ups/samples/sample-1.js
+++ pages/code-snippet-design-mock-ups/samples/sample-1.js
@@ -3,122 +3,115 @@ import styles from './Styles1.module.css'
 export default function Sample1() {
   return (
     <div>
-    <pre className={styles.background}>
-      <div className={styles.row}>
-        <span className={`${styles.line_number} ${styles.top_line}`}>1</span>
-        <span className={styles.line_content}>
-          <span className="token keyword">function</span>
-          <span className="token plain"> </span>
-          <span className="token function">get_random_number</span>
-          <span className="token punctuation">(</span>
-          <span className="token parameter">max_number</span>
-          <span className="token punctuation">)</span>
-          <span className="token plain"> </span>
-          <span className="token punctuation">{'{'}</span>
-          <span className="token plain"></span>
-        </span>
-      </div>
-
-      <div className={styles.row}>
-        <span className={styles.line_number}>2</span>
-        <span className={styles.line_content}>
-          <span className="token plain">&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <span className="token keyword">return</span>
-          <span className="token plain"> Math</span>
-          <span className="token punctuation">.</span>
-          <span className="token function">floor</span>
-          <span className="token punctuation">(</span>
-        </span>
-      </div>
-
-      <div className={styles.row}>
-        <span className={styles.line_number}>3</span>
-        <span className={styles.line_content}>
-          <span className="token plain">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <pre className={styles.background}>
+        <div className={styles.row}>
+          <span className={`${styles.line_content} ${styles.first_line}`}>
+            <span className="token keyword">function</span>
+            <span className="token plain"> </span>
+            <span className="token function">get_random_number</span>
+            <span className="token punctuation">(</span>
+            <span className="token parameter">max_number</span>
+            <span className="token punctuation">)</span>
+            <span className="token plain"> </span>
+            <span className="token punctuation">{'{'}</span>
+            <span className="token plain"></span>
           </span>
-          <span className="token punctuation">(</span>
-          <span className="token plain">Math</span>
-          <span className="token punctuation">.</span>
-          <span className="token function">random</span>
-          <span className="token punctuation">(</span>
-          <span className="token punctuation">)</span>
-          <span className="token plain"> </span>
-          <span className="token operator">*</span>
-          <span className="token plain"> max_number</span>
-          <span className="token punctuation">)</span>
-          <span className="token plain"> </span>
-          <span className="token operator">+</span>
-          <span className="token plain"> </span>
-          <span className="token number">1</span>
-        </span>
-      </div>
+        </div>
 
-      <div className={styles.row}>
-        <span className={styles.line_number}>4</span>
-        <span className={styles.line_content}>
-          <span className="token plain">&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <span className="token punctuation">)</span>
-        </span>
-      </div>
+        <div className={styles.row}>
+          <span className={styles.line_content}>
+            <span className="token plain">&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span className="token keyword">return</span>
+            <span className="token plain"> Math</span>
+            <span className="token punctuation">.</span>
+            <span className="token function">floor</span>
+            <span className="token punctuation">(</span>
+          </span>
+        </div>
 
-      <div className={styles.row}>
-        <span className={styles.line_number}>5</span>
-        <span className={styles.line_content}>
-          <span className="token plain"></span>
-          <span className="token punctuation">{'}'}</span>
-        </span>
-      </div>
+        <div className={styles.row}>
+          <span className={styles.line_content}>
+            <span className="token plain">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </span>
+            <span className="token punctuation">(</span>
+            <span className="token plain">Math</span>
+            <span className="token punctuation">.</span>
+            <span className="token function">random</span>
+            <span className="token punctuation">(</span>
+            <span className="token punctuation">)</span>
+            <span className="token plain"> </span>
+            <span className="token operator">*</span>
+            <span className="token plain"> max_number</span>
+            <span className="token punctuation">)</span>
+            <span className="token plain"> </span>
+            <span className="token operator">+</span>
+            <span className="token plain"> </span>
+            <span className="token number">1</span>
+          </span>
+        </div>
 
-      <div className={styles.row}>
-        <span className={styles.line_number}>6</span>
-        <span className={styles.line_content}></span>
-      </div>
+        <div className={styles.row}>
+          <span className={styles.line_content}>
+            <span className="token plain">&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span className="token punctuation">)</span>
+          </span>
+        </div>
 
-      <div className={styles.row}>
-        <span className={styles.line_number}>7</span>
-        <span className={styles.line_content}>
-          <span className="token plain">console</span>
-          <span className="token punctuation">.</span>
-          <span className="token function">log</span>
-          <span className="token punctuation">(</span>
-          <span className="token function">get_random_number</span>
-          <span className="token punctuation">(</span>
-          <span className="token number">100</span>
-          <span className="token punctuation">)</span>
-          <span className="token punctuation">)</span>
-        </span>
-      </div>
+        <div className={styles.row}>
+          <span className={styles.line_content}>
+            <span className="token plain"></span>
+            <span className="token punctuation">{'}'}</span>
+          </span>
+        </div>
 
-      <div className={styles.row}>
-        <span className={`${styles.line_number} ${styles.last_line}`}>
-          &nbsp;
-        </span>
-        <span className={styles.line_content}>&nbsp;</span>
-      </div>
+        <div className={styles.row}>
+          <span className={styles.line_content}></span>
+        </div>
 
-      <div className={styles.row}>
-        <span className={`${styles.line_number} ${styles.last_line}`}>
-          &nbsp;
-        </span>
-        <span className={styles.line_content}>{`// output`}</span>
-      </div>
+        <div className={styles.row}>
+          <span className={styles.line_content}>&nbsp;</span>
+        </div>
 
-      <div className={styles.output_line}>
-        <span className={styles.line_number_output}>&gt;</span>
-        <span className={styles.line_content}>27</span>
-      </div>
-      <button className={styles.copy_button}>Copy</button>
-    </pre>
+        <div className={styles.row}>
+          <span className={styles.line_content}>
+            <span className="token plain">console</span>
+            <span className="token punctuation">.</span>
+            <span className="token function">log</span>
+            <span className="token punctuation">(</span>
+            <span className="token function">get_random_number</span>
+            <span className="token punctuation">(</span>
+            <span className="token number">100</span>
+            <span className="token punctuation">)</span>
+            <span className="token punctuation">)</span>
+          </span>
+        </div>
 
-        <ul>
-          <li>Pretty much a baseline sample</li>
-          <li>Only showing ling numbers by the code.</li>
-          <li>
-            Using a pointer for the output line, but it&apos;s still in the main
-            content box
-          </li>
-        </ul>
+        <div className={styles.row}>
+          <span className={styles.line_content}>&nbsp;</span>
+        </div>
+
+        <div className={styles.row}>
+          <span className={styles.line_content}>{`// Output:`}</span>
+        </div>
+
+        <div className={styles.output_line}>
+          <span className={`${styles.line_content} ${styles.last_line}`}>
+            // 27
+          </span>
+        </div>
+
+        <button className={styles.copy_button}>Copy</button>
+      </pre>
+
+      <ul className="mt-8">
+        <li>Pretty much a baseline sample</li>
+        <li>Only showing ling numbers by the code.</li>
+        <li>
+          Using a pointer for the output line, but it&apos;s still in the main
+          content box
+        </li>
+      </ul>
     </div>
   )
 }
