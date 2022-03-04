51020fd332353933e25809a1fed9c65b8ef1ef5e
Alan Smith <alan@alanwsmith.com>
2022-03-04 10:29:32 -0500

    Add script to add another design in the middle and move the rest down the line

--- pages/code-snippet-design-mock-ups/add_design.py
+++ pages/code-snippet-design-mock-ups/add_design.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
-from os import listdir
+import os
+import shutil
 
-
-number_to_bump = 8
+number_to_bump = 1
 
 source_dir = 'designs'
-dir_list = [int(d) for d in listdir(source_dir) if d[0] != '.']
+dir_list = [int(d) for d in os.listdir(source_dir) if d[0] != '.']
 dir_list.sort()
 
-for num in range(len(dir_list), -1, -1):
-    if num == number_to_bump:
-        break
-    print(num)
+# move the directories forward one
+for num in range(len(dir_list), number_to_bump, -1):
+    original_path = f"{source_dir}/{str(num)}"
+    dest_path = f"{source_dir}/{str(num + 1)}"
+    print("Rename: ")
+    print(original_path)
+    print(dest_path)
+    os.rename(original_path, dest_path)
 
+
+
+dir_to_copy = f"{source_dir}/{number_to_bump}"
+dir_to_paste = f"{source_dir}/{number_to_bump + 1}"
+shutil.copytree(dir_to_copy, dir_to_paste)
+
+print(dir_to_copy)
+print(dir_to_paste)
