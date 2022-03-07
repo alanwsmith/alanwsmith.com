0c12933c8d1f655be0b3259e7e50df9443a26349
Alan Smith <alan@alanwsmith.com>
2022-03-07 17:13:39 -0500

    Cleaned up some posts

---
+++ _backup_images/_README.md
@@ -0,0 +1,4 @@
+These are copies of images that I link to on other sites. 
+
+I'm keeping a copy in case the originals disappear
+and I can't fine replacements. 
--- components/Img.js
+++ components/Img.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 
+import absolute_sandman from '../_images/absolute_sandman.jpeg'
 import acme_bag_2008_05 from '../_images/acme_bag_2008_05.jpg'
 import alligator_on_the_golf_course from '../_images/alligator_on_the_golf_course.jpg'
 import armored_bug_20060923_105004_01 from '../_images/armored_bug_20060923_105004_01.jpg'
 import aws_20080426_glamor_camp_0073 from '../_images/aws_20080426_glamor_camp_0073.jpg'
 import bama_schedule_sp_20090821_misc_112136 from '../_images/bama_schedule_sp_20090821_misc_112136.png'
+import beer_bottle_in_a_freezer_with_the_top_missing_and_frozen_beer_leaking_out_20060205_222340a from '../_images/beer_bottle_in_a_freezer_with_the_top_missing_and_frozen_beer_leaking_out_20060205_222340a.jpg'
 import big_ass_grasshopper_20070721_183012_01 from '../_images/big_ass_grasshopper_20070721_183012_01.jpg'
 import black_widow_20051026_230734a1 from '../_images/black_widow_20051026_230734a1.jpg'
 import bottle_of_change_20060313_150636a from '../_images/bottle_of_change_20060313_150636a.jpg'
@@ -33,34 +35,45 @@ import ir_band_photo_20060805_011816_01 from '../_images/ir_band_photo_20060805_
 import kexp_song_of_the_day_2009_06 from '../_images/kexp_song_of_the_day_2009_06.jpg'
 import last_sunday_at_reimers_img_0289 from '../_images/last_sunday_at_reimers_img_0289.jpg'
 import last_sunday_at_reimers_img_0293 from '../_images/last_sunday_at_reimers_img_0293.jpg'
+import logo_sample from '../_images/logo_sample.gif'
 import macbook_pro_2009_01 from '../_images/macbook_pro_2009_01.jpg'
+import max_logo from '../_images/max_logo.gif'
 import mile_200000_20080402_snaps_0039 from '../_images/mile_200000_20080402_snaps_0039.jpg'
 import movies_cropped_2006_02 from '../_images/movies_cropped_2006_02.jpg'
+import perl_camel from '../_images/perl_camel.png'
 import play_button_2009_01 from '../_images/play_button_2009_01.gif'
 import plr_sample_finish from '../_images/plr_sample_finish.jpg'
 import plr_sample_start from '../_images/plr_sample_start.jpg'
 import podcast_amazon_music_button from '../_images/podcast_amazon_music_button.png'
+import raw_image_20060401_124150a_fixed from '../_images/raw_image_20060401_124150a_fixed.jpg'
+import raw_image_20060401_124150a_original from '../_images/raw_image_20060401_124150a_original.jpg'
 import shuttle_patch_150x166 from '../_images/shuttle_patch_150x166.png'
+import skunk_works_logo from '../_images/skunk_works_logo.png'
 import snow_in_atlanta_dscf0874 from '../_images/snow_in_atlanta_dscf0874.jpg'
 import sunset_in_key_weat_20060211_191750a from '../_images/sunset_in_key_weat_20060211_191750a.jpg'
 import superfad_reel_grab_2009_01 from '../_images/superfad_reel_grab_2009_01.png'
 import target_with_bullet_holes_20060923_124436_01 from '../_images/target_with_bullet_holes_20060923_124436_01.jpg'
 import thepodofalan_ogimage_v1 from '../_images/thepodofalan_ogimage_v1.png'
+import thermometer_that_was_in_a_car_20070906_0001 from '../_images/thermometer_that_was_in_a_car_20070906_0001.jpg'
 import tree_at_sawgrass_20060329_041114a from '../_images/tree_at_sawgrass_20060329_041114a.jpg'
+import trinity from '../_images/trinity.jpg'
 import unprocessed_file_20060125_010408a from '../_images/unprocessed_file_20060125_010408a.jpg'
 import vivitar_285_hack_20060710_212242_01 from '../_images/vivitar_285_hack_20060710_212242_01.jpg'
 import vivitar_285_hack_2_20060710_212406_01 from '../_images/vivitar_285_hack_2_20060710_212406_01.jpg'
 import wedding_dance_20080301_0965 from '../_images/wedding_dance_20080301_0965.jpg'
+import wire_on_the_bottom_of_my_chair_20060921_131744_01 from '../_images/wire_on_the_bottom_of_my_chair_20060921_131744_01.jpg'
 import wireless_flash_mod_20060430_234118a from '../_images/wireless_flash_mod_20060430_234118a.jpg'
 import wobble_wedge_2009_02 from '../_images/wobble_wedge_2009_02.jpg'
 import youtube_logo_2009_01 from '../_images/youtube_logo_2009_01.gif'
 
 const imgMap = {
+  absolute_sandman: absolute_sandman,
   acme_bag_2008_05: acme_bag_2008_05,
   alligator_on_the_golf_course: alligator_on_the_golf_course,
   armored_bug_20060923_105004_01: armored_bug_20060923_105004_01,
   aws_20080426_glamor_camp_0073: aws_20080426_glamor_camp_0073,
   bama_schedule_sp_20090821_misc_112136: bama_schedule_sp_20090821_misc_112136,
+  beer_bottle_in_a_freezer_with_the_top_missing_and_frozen_beer_leaking_out_20060205_222340a: beer_bottle_in_a_freezer_with_the_top_missing_and_frozen_beer_leaking_out_20060205_222340a,
   big_ass_grasshopper_20070721_183012_01: big_ass_grasshopper_20070721_183012_01,
   black_widow_20051026_230734a1: black_widow_20051026_230734a1,
   bottle_of_change_20060313_150636a: bottle_of_change_20060313_150636a,
@@ -89,24 +102,33 @@ const imgMap = {
   kexp_song_of_the_day_2009_06: kexp_song_of_the_day_2009_06,
   last_sunday_at_reimers_img_0289: last_sunday_at_reimers_img_0289,
   last_sunday_at_reimers_img_0293: last_sunday_at_reimers_img_0293,
+  logo_sample: logo_sample,
   macbook_pro_2009_01: macbook_pro_2009_01,
+  max_logo: max_logo,
   mile_200000_20080402_snaps_0039: mile_200000_20080402_snaps_0039,
   movies_cropped_2006_02: movies_cropped_2006_02,
+  perl_camel: perl_camel,
   play_button_2009_01: play_button_2009_01,
   plr_sample_finish: plr_sample_finish,
   plr_sample_start: plr_sample_start,
   podcast_amazon_music_button: podcast_amazon_music_button,
+  raw_image_20060401_124150a_fixed: raw_image_20060401_124150a_fixed,
+  raw_image_20060401_124150a_original: raw_image_20060401_124150a_original,
   shuttle_patch_150x166: shuttle_patch_150x166,
+  skunk_works_logo: skunk_works_logo,
   snow_in_atlanta_dscf0874: snow_in_atlanta_dscf0874,
   sunset_in_key_weat_20060211_191750a: sunset_in_key_weat_20060211_191750a,
   superfad_reel_grab_2009_01: superfad_reel_grab_2009_01,
   target_with_bullet_holes_20060923_124436_01: target_with_bullet_holes_20060923_124436_01,
   thepodofalan_ogimage_v1: thepodofalan_ogimage_v1,
+  thermometer_that_was_in_a_car_20070906_0001: thermometer_that_was_in_a_car_20070906_0001,
   tree_at_sawgrass_20060329_041114a: tree_at_sawgrass_20060329_041114a,
+  trinity: trinity,
   unprocessed_file_20060125_010408a: unprocessed_file_20060125_010408a,
   vivitar_285_hack_20060710_212242_01: vivitar_285_hack_20060710_212242_01,
   vivitar_285_hack_2_20060710_212406_01: vivitar_285_hack_2_20060710_212406_01,
   wedding_dance_20080301_0965: wedding_dance_20080301_0965,
+  wire_on_the_bottom_of_my_chair_20060921_131744_01: wire_on_the_bottom_of_my_chair_20060921_131744_01,
   wireless_flash_mod_20060430_234118a: wireless_flash_mod_20060430_234118a,
   wobble_wedge_2009_02: wobble_wedge_2009_02,
   youtube_logo_2009_01: youtube_logo_2009_01,
