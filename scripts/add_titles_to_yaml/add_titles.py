1ab8a36274dcd467f6722940aebfbdce939ba092
Alan Smith <commit@alanwsmith.com>
2022-05-14 00:44:52 -0400

    Start a basic test setup to add yaml data

--- scripts/add_titles_to_yaml/add_titles.py
+++ scripts/add_titles_to_yaml/add_titles.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
 
+import os
 import re
+import yaml
 
 from list_dir import list_dir
 
-source_path = '/Users/alan/Library/Mobile Documents/com~apple~CloudDocs/Grimoire'
+source_dir = '/Users/alan/Library/Mobile Documents/com~apple~CloudDocs/Grimoire'
+output_dir = 'output'
 
-file_list = list_dir(root_dir=source_path)
 
-for file in file_list:
-    if file['extension'] == 'txt':
-        if file['sub_dir'] == '':
-            file_path = file['full_path']
-            # print(file_path)
+def add_titles(original_data, file_name):
+    pass
 
-            with open(file_path) as _in:
-                full_file = _in.read()
-                file_parts = full_file.split('---')
-                if len(file_parts) > 2:
-                    if re.search(r'id: ', file_parts[1]):
-                        if not re.search(r'title: ', file_parts[1]):
-                            print(file_path)
+
+def update_yaml(data, file_name):
+    file_name_parts = file_name.split('-')
+    data['title'] = '-'.join(file_name_parts[1:]).strip()
+    return data
+
+
+# file_list = list_dir(root_dir=source_dir)
+# for file in file_list:
+#     if file['extension'] == 'txt':
+#         if file['sub_dir'] == '':
+#             source_path = file['full_path']
+#             # print(file_path)
+#             with open(source_path) as _in:
+#                 full_file = _in.read()
+#                 file_parts = full_file.split('---')
+#                 if len(file_parts) > 2:
+#                     if re.search(r'id: ', file_parts[1]):
+#                         if not re.search(r'title: ', file_parts[1]):
+#                             print(source_path)
+#                             output_path = os.path.join(output_dir, file['name_with_extension'])
+#                             print(output_path)
+#                             yaml_data = yaml.load(file_parts[1], Loader=yaml.FullLoader)
+#                             print(yaml_data)
+#                             # with open(output_path, 'w') as _out:
+#                             #     _out.write('---'.join(file_parts))
 
 
 
---
+++ scripts/add_titles_to_yaml/test_add_titles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from add_titles import add_titles
+from add_titles import update_yaml
+
+class AddTitleTest(unittest.TestCase):
+
+    def test_update_yaml(self):
+        test_data = { "id": "1234" }
+        test_file_name = "py- This Is A Test - With A Dash"
+        expected = 'This Is A Test - With A Dash'
+        result = update_yaml(test_data, test_file_name)['title']
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
+
