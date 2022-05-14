678f9efaed9cc7757f1125f70e9aac437b6d7ce9
Alan Smith <commit@alanwsmith.com>
2022-05-14 13:38:00 -0400

    Making the base slug

--- scripts/deployer/lib/make_slug.mjs
+++ scripts/deployer/lib/make_slug.mjs
@@ -1,7 +1,21 @@
 import { parse } from 'yaml'
 
 function make_slug(frontmatter) {
-  return '20eLy6aaaaaa--open-source-test-thing'
+  // TODO: Error if there is no title
+
+  const data = parse(frontmatter)
+
+  const dash_replace = /-+/g
+  let base_title = data['title']
+    .replaceAll(' ', '-')
+    .replace(dash_replace, '-')
+    .toLowerCase()
+
+  let slug = `${data['id']}--${base_title}`
+
+  console.log(slug)
+
+  return slug
 }
 
 export default make_slug
