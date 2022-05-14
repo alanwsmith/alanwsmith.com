d83553321d37509c587ef2a14bc0a1e7ab584762
Alan Smith <commit@alanwsmith.com>
2022-05-14 19:10:43 -0400

    Adding slug to the redirect storage

---
+++ scripts/deployer/test/test_add_slug_to_redirect_storage.mjs
@@ -0,0 +1,32 @@
+import assert from 'assert'
+import add_slug_to_redirect_storage from '../lib/add_slug_to_redirect_storage.mjs'
+
+it('should add the slug to the list', () => {
+  // Given
+  const json_data = {
+    redirect_data: {
+      '29ajjouaaaaa': {
+        redirect_from: ['/posts/29ajjouaaaaa'],
+      },
+    },
+  }
+
+  const slug = 'this-is-new--29ajjouaaaaa'
+
+  // When
+  let result = {
+    redirect_data: {
+      '29ajjouaaaaa': {
+        redirect_from: ['/posts/29ajjouaaaaa', 'this-is-new--29ajjouaaaaa'],
+      },
+    },
+  }
+
+  result = add_slug_to_redirect_storage({ json_data: json_data, slug: slug })
+
+  // Then
+  assert.equal(
+    result.redirect_data['29ajjouaaaaa'].redirect_from[1],
+    'this-is-new--29ajjouaaaaa'
+  )
+})
