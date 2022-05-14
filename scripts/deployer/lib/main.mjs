7000685fcc3566f822ae1e6b38526f733f986361
Alan Smith <commit@alanwsmith.com>
2022-05-14 14:37:29 -0400

    Starting integratino test

--- scripts/deployer/lib/main.mjs
+++ scripts/deployer/lib/main.mjs
@@ -1,4 +1,17 @@
+import fs from 'fs'
+
 const main = () => {
+  // TODO: Only process files that have an ID in the yaml front matter
+  const file_data = fs.readFileSync(
+    'test/_input/samples_a/py- Some Thing - Here.txt',
+    'utf-8'
+  )
+  console.log(file_data)
+
+  fs.writeFileSync(
+    'test/_output/a/posts/do-something-in-python--29ajjouaaaaa.mdx',
+    'data'
+  )
   console.log('HERE')
 }
 
--- scripts/deployer/test/test_main_integration.mjs
+++ scripts/deployer/test/test_main_integration.mjs
@@ -3,9 +3,28 @@ import fs from 'fs'
 import main from '../lib/main.mjs'
 
 it('makes the output file', () => {
-  main({})
-  const does_it_exist = fs.existsSync(
-    'test/_output/a/posts/do-something-in-python--29ajjouaaaaa.mdx'
-  )
-  assert.equal(does_it_exist, true)
+  // Given
+  const output_files = [
+    'test/_output/a/posts/do-something-in-python--29ajjouaaaaa.mdx',
+  ]
+
+  output_files.forEach((file_path) => {
+    try {
+      fs.unlinkSync(file_path)
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
+  // When
+  main({
+    input_dir: 'test/_input/samples_a',
+    output_posts_dir: 'test/_output/a/posts',
+  })
+
+  // Then
+  output_files.forEach((file_path) => {
+    const does_it_exist = fs.existsSync(file_path)
+    assert.equal(does_it_exist, true)
+  })
 })
