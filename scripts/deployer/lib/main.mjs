a9f6ef409fba5aa60df4aa4893994ff9f6c1db3e
Alan Smith <commit@alanwsmith.com>
2022-05-14 18:18:31 -0400

    Processing input directory

--- scripts/deployer/lib/main.mjs
+++ scripts/deployer/lib/main.mjs
@@ -1,30 +1,35 @@
 import fs from 'fs'
 import { parse, stringify } from 'yaml'
 import add_slug_to_frontmatter_object from './add_slug_to_frontmatter_object.mjs'
+import list_dir from './list_dir.mjs'
 
 const main = ({ input_posts_dir, output_posts_dir }) => {
   // TODO: Only process files that have an
   // ID in the yaml front matter
+  //
+  //
+  // TODO: Loop over input dir for files.
+  //
 
-  const file_data = fs.readFileSync(
-    `${input_posts_dir}/py- Some Thing - Here.txt`,
-    'utf-8'
-  )
+  const file_list = list_dir(input_posts_dir)
 
-  const file_parts = file_data.split('---')
+  file_list.forEach((file) => {
+    const file_data = fs.readFileSync(
+      `${input_posts_dir}/py- Some Thing - Here.txt`,
+      'utf-8'
+    )
 
-  let frontmatter = parse(file_parts[1])
-  console.log(frontmatter)
+    const file_parts = file_data.split('---')
+    let frontmatter = parse(file_parts[1])
+    frontmatter = add_slug_to_frontmatter_object(frontmatter)
 
-  frontmatter = add_slug_to_frontmatter_object(frontmatter)
-  console.log(frontmatter)
-
-  file_parts[1] = `
+    file_parts[1] = `
 ${stringify(frontmatter)}`
 
-  const output_file_path = `test/_output/a/posts/${frontmatter.slug}.mdx`
+    const output_file_path = `test/_output/a/posts/${frontmatter.slug}.mdx`
 
-  fs.writeFileSync(output_file_path, file_parts.join('---'))
+    fs.writeFileSync(output_file_path, file_parts.join('---'))
+  })
 }
 
 export default main
