a003262c76d8b44fffc870523d1b52b2de5fb702
Alan Smith <commit@alanwsmith.com>
2022-05-15 17:38:38 -0400

    Finished first version of the redirect migration script

--- scripts/redirect_migrator/migrate_redirects.js
+++ scripts/redirect_migrator/migrate_redirects.js
@@ -20,14 +20,35 @@ for (const [key, value] of Object.entries(fileData.ksuid_redirects)) {
 
   output[key].redirect_from.push(value.current_slug)
   output[key].redirect_from.push(`/${key}`)
-  output[key].redirect_from.push(`/post/${key}`)
+  output[key].redirect_from.push(`/posts/${key}`)
 
   value.slugs_to_redirect.forEach((slug) => {
     output[key].redirect_from.push(slug)
   })
 }
 
-// console.log(output)
+const redirectsToSplit = fs.readFileSync('input/_redirects', 'utf8')
+
+const redirectLines = redirectsToSplit.split('\n')
+
+redirectLines.forEach((line) => {
+  const lineParts = line.split(/\s+/)
+  console.log(`A: ${lineParts[0]} - ${lineParts[1]}`)
+
+  // get the id
+  const urlParts = lineParts[1].split('/')
+  if (urlParts.length > 2) {
+    const slugParts = urlParts[2].split('--')
+    const key = slugParts[0]
+    console.log(key)
+    if (output[key].redirect_from.indexOf(lineParts[0]) === -1) {
+      output[key].redirect_from.push(lineParts[0])
+    }
+    if (output[key].redirect_from.indexOf(lineParts[1]) === -1) {
+      output[key].redirect_from.push(lineParts[1])
+    }
+  }
+})
 
 fs.writeFileSync(
   'output/redirect_stroage.json',
