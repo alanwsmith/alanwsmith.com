a25b72fa37065478287979fa94ff534f83d243c0
Alan Smith <commit@alanwsmith.com>
2022-05-15 18:44:00 -0400

    Started to put in a redirect for missing "to" urls to the home page, but isn't necessary here

--- scripts/deployer/lib/make_redirects_array.mjs
+++ scripts/deployer/lib/make_redirects_array.mjs
@@ -3,6 +3,23 @@ function make_redirects_array({ json_data }) {
 
   for (const post_id in json_data.redirect_data) {
     json_data.redirect_data[post_id].redirect_from.forEach((from_location) => {
+      // // Redirct to home page if the page has been removed
+      // // and no longer has a URL (NOTE: I don't think this actually
+      // // gets called since if the page isn't there, it won't have
+      // // a reference to start with. Remove this at some point
+      // // after confirming)
+      // if (json_data.redirect_data[post_id].redirect_to === '') {
+      //   results.push({
+      //     from: from_location,
+      //     to: '/',
+      //   })
+      // }
+
+      // otherwise, add the redirect as long as it's not
+      // pointing to itself
+      //
+      // else
+
       if (from_location !== json_data.redirect_data[post_id].redirect_to) {
         results.push({
           from: from_location,
