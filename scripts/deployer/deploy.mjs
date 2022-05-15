a8309813dd1cd401a72218549f8f4eeeb1789ec0
Alan Smith <commit@alanwsmith.com>
2022-05-15 19:04:21 -0400

    Configured dev and prod separately

--- scripts/deployer/deploy.mjs
+++ scripts/deployer/deploy.mjs
@@ -2,21 +2,34 @@
 
 import { main } from './lib/main.mjs'
 
-function deploy_site() {
+function deploy_site(env) {
   const config = {
-    input_posts_dir:
-      '/Users/alan/Library/Mobile Documents/com~apple~CloudDocs/Grimoire',
-    output_posts_dir:
-      '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/posts',
-    redirect_storage_input:
-      '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_input/_prod_test/data/redirect_storage.json',
-    redirect_storage_output:
-      '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/data/redirect_storage.json',
-    redirect_file_path:
-      '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/data/_redirects',
+    dev: {
+      input_posts_dir:
+        '/Users/alan/Library/Mobile Documents/com~apple~CloudDocs/Grimoire',
+      output_posts_dir:
+        '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/posts',
+      redirect_storage_input:
+        '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_input/_prod_test/data/redirect_storage.json',
+      redirect_storage_output:
+        '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/data/redirect_storage.json',
+      redirect_file_path:
+        '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/data/_redirects',
+    },
+    prod: {
+      input_posts_dir:
+        '/Users/alan/Library/Mobile Documents/com~apple~CloudDocs/Grimoire',
+      output_posts_dir: '/Users/alan/workshop/alanwsmith.com/site/_posts',
+      redirect_storage_input:
+        '/Users/alan/workshop/alanwsmith.com/site/_data/redirect_storage.json',
+      redirect_storage_output:
+        '/Users/alan/workshop/alanwsmith.com/site/_data/redirect_storage.json',
+      redirect_file_path:
+        '/Users/alan/workshop/alanwsmith.com/site/_data/_redirect',
+    },
   }
 
-  main(config)
+  main(config[env])
 }
 
-deploy_site()
+deploy_site('dev')
