b87561e298253305dc025331b8f1d380676ea857
Alan Smith <commit@alanwsmith.com>
2022-05-14 23:35:19 -0400

    Basic first run in place

--- scripts/deployer/deploy.js
+++
@@ -1,5 +0,0 @@
-function makeFileName(frontmatter) {
-  return 
-
-
-}
---
+++ scripts/deployer/deploy.mjs
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { main } from './lib/main.mjs'
+
+function deploy_site() {
+  const config = {
+    input_posts_dir:
+      '/Users/alan/Library/Mobile Documents/com~apple~CloudDocs/Grimoire',
+    output_posts_dir:
+      '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/posts',
+    redirect_storage_input:
+      '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_input/samples_a/data/redirect_storage.json',
+    redirect_storage_output:
+      '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/data/redirect_storage.json',
+    redirect_file_path:
+      '/Users/alan/workshop/alanwsmith.com/scripts/deployer/test/_output/_prod_test/data/_redirects',
+  }
+
+  main(config)
+}
+
+deploy_site()
