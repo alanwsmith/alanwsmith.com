d953120cd3b69f5506eff5be88bfd37943817a64
Alan Smith <commit@alanwsmith.com>
2022-05-14 22:51:30 -0400

    Dev version that ignore front matter is ready

--- scripts/deployer/lib/main.mjs
+++ scripts/deployer/lib/main.mjs
@@ -67,4 +67,69 @@ ${stringify(frontmatter)}`
   fs.writeFileSync(redirect_file_path, redirect_string)
 }
 
-export { main }
+function main_dev({
+  input_posts_dir,
+  output_posts_dir,
+  redirect_storage_input,
+  redirect_storage_output,
+  redirect_file_path,
+}) {
+  // TODO: Only process files that have an
+  // ID in the yaml front matter
+  // TODO: Only process .txt files.
+
+  const file_list = list_dir({ rootDir: input_posts_dir, isRecursive: false })
+
+  // Load redirect_storage json
+  let redirect_data = JSON.parse(
+    fs.readFileSync(redirect_storage_input, 'utf-8')
+  )
+
+  file_list.forEach((file) => {
+    console.log(`Processing: ${file.full_path}`)
+    const file_data = fs.readFileSync(file.full_path, 'utf-8')
+
+    const file_parts = file_data.split('---')
+
+    if (file_parts.length > 1) {
+      let frontmatter = parse(file_parts[1])
+      // slug is assigned here
+      frontmatter = add_slug_to_frontmatter_object(frontmatter)
+
+      // This is to deal with adding a space
+      // before the first '---' set
+      file_parts[1] = `
+${stringify(frontmatter)}`
+
+      // let output_file_path = `test/_output/a/posts/${frontmatter.slug}.mdx`
+      const output_file_path = `${output_posts_dir}/${frontmatter.slug}.mdx`
+      const url = `/posts/${frontmatter.slug}`
+
+      fs.writeFileSync(output_file_path, file_parts.join('---'))
+
+      // Update redirects_stroage.json
+      redirect_data = add_url_to_redirect_storage({
+        json_data: redirect_data,
+        url: url,
+      })
+    }
+  })
+
+  // output updated redirect_storage json
+  fs.writeFileSync(
+    redirect_storage_output,
+    JSON.stringify(redirect_data, null, 2)
+  )
+
+  // create _redirects file
+  const redirects_array = make_redirects_array({ json_data: redirect_data })
+  const tmp_redirect_data = []
+  redirects_array.forEach((item) => {
+    tmp_redirect_data.push(`${item.from}    ${item.to}    301`)
+  })
+  const redirect_string = tmp_redirect_data.join('\n')
+  console.log(redirect_string)
+  fs.writeFileSync(redirect_file_path, redirect_string)
+}
+
+export { main, main_dev }
--- scripts/deployer/test/test_main_integration.mjs
+++ scripts/deployer/test/test_main_integration.mjs
@@ -1,6 +1,7 @@
 import assert from 'assert'
 import fs from 'fs'
 import { main } from '../lib/main.mjs'
+import { main_dev } from '../lib/main.mjs'
 
 it('makes the output file', () => {
   // Given
@@ -30,4 +31,20 @@ it('makes the output file', () => {
     const does_it_exist = fs.existsSync(file_path)
     assert.equal(does_it_exist, true)
   })
+})
+
+it('does not crash if there is no front matter', () => {
+  // When
+  main_dev({
+    input_posts_dir: 'test/_input/samples_b/posts',
+    output_posts_dir: 'should not matter for this test',
+    redirect_storage_input: 'test/_input/samples_b/data/redirect_storage.json',
+    redirect_storage_output: 'should not matter for this test',
+    redirect_file_path: 'should not matter for this test',
+  })
+
+  // This just makes sure that the process gets here.
+  // (A crash is the error condition)
+  //
+  assert.equal(1, 1)
 })
